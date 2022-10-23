5282f4c719b726972db8da1d15fc4551b9889924
Clara Smyth <clarasmyth.work@gmail.com>
2022-10-23 17:34:28 +0100

    Updated ContactDetails component

--- src/components/ContactDetails.js
+++ src/components/ContactDetails.js
@@ -1,25 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export class contactDetails extends Component {
   render() {
+    const { details, updateDetails } = this.props;
+
     return (
-    <div>
-      <h2>Contact Details</h2>
+      <form className="form">
+        <h2>Contact Details</h2>
 
-      <label htmlFor='websiteInput'>Website</label>
-      <input id='websiteInput' name='websiteInput' type='text'></input>
+        <label htmlFor="websiteInput">Website</label>
+        <input
+          id="websiteInput"
+          name="websiteInput"
+          type="text"
+          onChange={updateDetails('website')}
+          defaultValue={details.website}
+        />
 
-      <label htmlFor='emailInput'>First Name</label>
-      <input id='emailInput' name='emailInput' type='text'></input>
+        <label htmlFor="emailInput">Email</label>
+        <input
+          id="emailInput"
+          name="emailInput"
+          type="text"
+          onChange={updateDetails('email')}
+          defaultValue={details.email}
+        />
 
-      <label htmlFor='locationInput'>Info</label>
-      <input id='locationInput' name='locationInput' type='text'></input>
+        <label htmlFor="locationInput">Location</label>
+        <input
+          id="locationInput"
+          name="locationInput"
+          type="text"
+          onChange={updateDetails('location')}
+          defaultValue={details.location}
+        />
 
-      <label htmlFor='phoneInput'>Info</label>
-      <input id='phoneInput' name='phoneInput' type='text'></input>
-    </div>
-    )
+        <label htmlFor="phoneInput">Phone</label>
+        <input
+          id="phoneInput"
+          name="phoneInput"
+          type="text"
+          onChange={updateDetails('phone')}
+          defaultValue={details.phone}
+        />
+        <button onClick={this.props.prevPage}>Prev</button>
+        <button onClick={this.props.nextPage}>Next</button>
+      </form>
+    );
   }
 }
 
-export default contactDetails
+export default contactDetails;
