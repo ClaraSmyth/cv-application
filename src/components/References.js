c5e66ce96603306c3190090f42c870e1f75d7059
Clara Smyth <clarasmyth.work@gmail.com>
2022-11-02 23:37:33 +0000

    Added References component

---
+++ src/components/References.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+export class References extends Component {
+  state = {
+    page: 1,
+  };
+
+  updatePage = (num) => (e) => {
+    e.preventDefault();
+    this.setState({ page: num });
+  };
+
+  render() {
+    const { details, updateDetails } = this.props;
+    const { page } = this.state;
+
+    return (
+      <form className="form">
+        <h2>References</h2>
+
+        <div>
+          <button onClick={this.updatePage(1)}>1</button>
+          <button onClick={this.updatePage(2)}>2</button>
+        </div>
+
+        <label htmlFor="websiteInput">Name</label>
+        <input
+          id="refNameInput"
+          name="refNameInput"
+          type="text"
+          onChange={updateDetails('references', `refName${page}`)}
+          defaultValue={details[`refName${page}`]}
+          key={`refNameInput:refName${page}`}
+        />
+
+        <label htmlFor="websiteInput">Phone</label>
+        <input
+          id="refPhoneInput"
+          name="refPhoneInput"
+          type="text"
+          onChange={updateDetails('references', `refPhone${page}`)}
+          defaultValue={details[`refPhone${page}`]}
+          key={`refPhoneInput:refPhone${page}`}
+        />
+
+        <label htmlFor="websiteInput">Email</label>
+        <input
+          id="refEmailInput"
+          name="refEmailInput"
+          type="text"
+          onChange={updateDetails('references', `refEmail${page}`)}
+          defaultValue={details[`refEmail${page}`]}
+          key={`refEmailInput:refEmail${page}`}
+        />
+
+        <div>
+          <button onClick={this.props.prevPage}>Prev</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default References;
